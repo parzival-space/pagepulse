9084de8e48da99575fa4f52c026c5d45505bd6a5
Malte Linke <me@parzival.space>
2023-03-31 10:38:51 +0200

    added properties documentation

--- src/main/java/space/parzival/pagepulse/properties/DatabaseProperties.java
+++ src/main/java/space/parzival/pagepulse/properties/DatabaseProperties.java
@@ -1,19 +1,39 @@
 package space.parzival.pagepulse.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
-@Component
+@Configuration
 @ConfigurationProperties(prefix = "pagepulse.database")
 public class DatabaseProperties {
+
+  /**
+   * The maximum time in seconds until a SQL query fails.
+   */
   private int queryTimeout = 30;
+
+  /**
+   * A JDBC connection string.
+   * Example: jdbc:sqlite:./database.db
+   */
   private String connection = "";
+
+  /**
+   * A prefix that will be prepended to every table created by this app.
+   */
   private String tablePrefix = "pagepulse_";
 
+  /**
+   * The interval in seconds in which this app removed old entries from the history.
+   */
   private int cleanupInterval = 60;
+
+  /**
+   * The maximum number of allowed entries in the history table.
+   */
   private int entryLimit = 10;
 }
--- src/main/java/space/parzival/pagepulse/properties/SocialProperties.java
+++ src/main/java/space/parzival/pagepulse/properties/SocialProperties.java
@@ -10,6 +10,17 @@ import lombok.Setter;
 @Component
 @ConfigurationProperties(prefix = "pagepulse.social")
 public class SocialProperties {
+  /**
+   * The name of your GitHub account or a repository.
+   * This app will prepend 'https://github.com/' to whatever value you provide.
+   * Disabled if not specified.
+   */
   private String github = null;
+
+  /**
+   * The id of your Discord user.
+   * This app will generate a direct link to your Discord account.
+   * Disabled if not specified.
+   */
   private String discord = null;
 }
